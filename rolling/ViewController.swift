0fa1e179bbb0053cb42771b094929d963d2da50f
Jamie Walker <jw6316@gmail.com>
2015-12-04 19:23:26 +0900

    Initial Commit

---
+++ rolling/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  rolling
+//
+//  Created by Jamie Walker on 12/4/15.
+//  Copyright © 2015 Jamie Walker. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
